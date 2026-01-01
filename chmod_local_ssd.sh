72d1a76599e8d1fc587626b0915b4b393e49cfe6
Passer-ByB <gaopeng.pb@gmail.com>
2026-01-01 02:57:39 +0000

    Fix module dependency problems that makes start-up flaky.
    This makes the fstab mount points `nofail` to avoid a local SSD
    induced boot failure. This is because setup_local_ssd makes
    assumption on device path convention. We don't want to fail
    booting the VM which is hard to recover from.
    
    Without hard dependency, we cannot synchronize chmod_local_ssd
    on local-fs.target. We make chmod_local_ssd directly depend on
    the local ssd mount points which depends on -.mount via
    setup_local_ssd. systemd-escape is a convenient way to get
    the mount unit name.
    
    Even after mount succeeds, the mount point might not be immediately
    visiable to chmod_local_ssd somehow, so we add retry logic to wait
    until the mount is visible.

--- chmod_local_ssd.sh
+++ chmod_local_ssd.sh
@@ -5,5 +5,16 @@ INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for i in $(seq 0 3); do
 	MOUNT_POINT=${INSTALL_DIR}/local_ssd_mnt_${i}
-	chmod 777 ${MOUNT_POINT}
+	RETRY_COUNT=1
+	while [ ${RETRY_COUNT} -le 60 ]
+	do
+		mountpoint -q ${MOUNT_POINT} && chmod 777 ${MOUNT_POINT}
+		if [ $? -ne 0 ]; then
+			echo "${MOUNT_POINT} is not a mount point. Current retry count: ${RETRY_COUNT}"
+			sleep 1
+			((RETRY_COUNT++))
+		else
+			break
+		fi
+	done
 done
--- install.sh
+++ install.sh
@@ -22,43 +22,45 @@ Type=oneshot
 ExecStart=${SETUP_SCRIPT_PATH}
 User=root
 Restart=on-failure
-RemainAfterExit=true
-
-[Install]
-WantedBy=local-fs.target
+RemainAfterExit=yes
 EOF
+
+# Configure /etc/fstab
+SSD_PATH_PREFIX="/dev/disk/by-id/google-local-nvme-ssd-"
+
+CHMOD_MOUNT_DEPENDENCY=""
+
+for i in $(seq 0 3); do
+  SSD_DEVICE="${SSD_PATH_PREFIX}${i}"
+  MOUNT_POINT="${INSTALL_DIR}/local_ssd_mnt_${i}"
+  mkdir ${MOUNT_POINT}
+  CHMOD_MOUNT_DEPENDENCY="${CHMOD_MOUNT_DEPENDENCY}$(systemd-escape ${MOUNT_POINT}).mount "
+
+  if ! grep -qs "${SSD_DEVICE}" /etc/fstab; then
+    # x-systemd.wants is not supported until systemd version 257
+    # using x-systemd.requires instead, but let's use nofail to avoid a boot failure if setup_local_ssd.service fails
+    echo "${SSD_DEVICE} ${MOUNT_POINT} ext4 defaults,nofail,x-systemd.requires=setup_local_ssd.service 0 1" | tee -a /etc/fstab
+  fi
+done
 
 # Create chmod_local_ssd service
 SYSTEMD_EDITOR=tee systemctl edit --force --full chmod_local_ssd.service <<EOF
 [Unit]
 Description=Make Local SSDs accessible by all users
-Requires=local-fs.target
-After=local-fs.target
+Requires=${CHMOD_MOUNT_DEPENDENCY}
+After=${CHMOD_MOUNT_DEPENDENCY}
 
 [Service]
 Type=oneshot
 ExecStart=$CHMOD_SCRIPT_PATH
 User=root
 Restart=on-failure
-RemainAfterExit=true
+RemainAfterExit=yes
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl enable setup_local_ssd
 systemctl enable chmod_local_ssd
 
-# Configure /etc/fstab
-SSD_PATH_PREFIX="/dev/disk/by-id/google-local-nvme-ssd-"
-
-for i in $(seq 0 3); do
-  SSD_DEVICE="${SSD_PATH_PREFIX}${i}"
-  MOUNT_POINT="${INSTALL_DIR}/local_ssd_mnt_${i}"
-  mkdir ${MOUNT_POINT}
-
-  if ! grep -qs "${SSD_DEVICE}" /etc/fstab; then
-    echo "${SSD_DEVICE} ${MOUNT_POINT} ext4 defaults,x-systemd.wants=setup_local_ssd.service 0 1" | tee -a /etc/fstab
-  fi
-done
 
